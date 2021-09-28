217015653939ab3dabd4f0ef15d394f46dd99ea9
WahyuNgrho <hallawahyu@gmail.com>
2021-09-28 12:31:44 +0700

    make table for data prayer times

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
+//* function fetch API from aladhan.com
+function prayerTimes(latitude, longitude) {
+    fetch('https://api.aladhan.com/v1/calendar?latitude=' + latitude + '&longitude=' + longitude + '&method=2&month=9&year=2021').then(response => response.json()).then(function (response) {
+        console.log(response);
+        let date = new Date();
+        let today = date.getDate() - 1;
+        let data = response.data[today].timings;
+
+        let app = document.getElementById('app');
+        let table = document.createElement('table');
+        let tableTbody = document.createElement('tbody');
+
+        for (i in data) {
+            let row = tableTbody.insertRow();
+            let name = row.insertCell(0);
+            let time = row.insertCell(1);
+
+            name.innerHTML = i;
+            time.innerHTML = data[i];
+            tableTbody.appendChild(row);
+        }
+        table.appendChild(tableTbody);
+        app.appendChild(table);
+
+        console.log(data);
+    });
+}
+
 //* function callback Geolocation
 function success(position) {
     console.log(position);
+    prayerTimes(position.coords.latitude, position.coords.longitude);
 }
 
 function error() {
